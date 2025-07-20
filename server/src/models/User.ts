758b6e3d7e118a55a66d30317914870bd662879d
Nicholas Bruno <165311112+Plasmatronics@users.noreply.github.com>
2025-07-19 21:32:23 -0400

    (improve) enhance user model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,22 +1,58 @@
-import mongoose from "mongoose";
-import zod from "zod";
+import mongoose, { Document } from "mongoose";
+import validator from "validator";
 
-const userSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: [true, "A user must have a name"],
-		unique: true,
-		trim: true,
-		maxlength: [30, "A name musn't exceed 30 characters."],
-		minlength: [7, "A name must exceed 78 characters."],
+interface UserInterface extends Document {
+	name: string;
+	email: string;
+	password: string;
+	createdAt: Date;
+	_passwordConfirm?: string;
+}
+
+const userSchema = new mongoose.Schema<UserInterface>(
+	{
+		name: {
+			type: String,
+			required: [true, "A user must have a name"],
+			unique: true,
+			trim: true,
+			maxlength: [30, "A name musn't exceed 30 characters."],
+			minlength: [7, "A name must exceed 7 characters."],
+		},
+		email: {
+			type: String,
+			required: [true, "A user must have an email"],
+			unique: true,
+			validate: [validator.isEmail, "Please provide a valid email"],
+		},
+		password: {
+			type: String,
+			required: [true, "A user must have a password"],
+			minlength: 8,
+			select: false,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now(),
+			select: false,
+		},
 	},
-	email: {
-		type: String,
-		required: [true, "A user must have an email"],
-		unique: true,
-		validate: [zod.email(), "Please provide a valid email"],
+	{
+		toJSON: { virtuals: true },
+		toObject: { virtuals: true },
 	},
+);
+
+userSchema.virtual("passwordConfirm").set(function (value: string) {
+	this._passwordConfirm = value;
 });
-const User = mongoose.model("User", userSchema);
 
+userSchema.pre("save", function (next) {
+	if (this.password !== this._passwordConfirm) {
+		return next(new Error("Passwords are not the same!"));
+	}
+	next();
+});
+
+const User = mongoose.model("User", userSchema);
 export default User;
