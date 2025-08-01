23fb064321ca8e1b66045157bb6a60cc764032cd
Nicholas Bruno <165311112+Plasmatronics@users.noreply.github.com>
2025-08-01 14:39:17 -0400

    (feat) implement security and compression benefits

--- server/index.ts
+++ server/index.ts
@@ -7,24 +7,63 @@ import {
 	mediaRouter,
 	storyRouter,
 	userRouter,
-	fileRouter
+	fileRouter,
 } from "./src/routes";
 import { AppError } from "./src/utils";
 import { globalErrorHandler } from "./src/controllers";
 import { fileURLToPath } from "url";
+import rateLimit from "express-rate-limit";
 import path from "path";
+import helmet from "helmet";
 import cookieParser from "cookie-parser";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
+import hpp from "hpp";
+import cors from "cors";
+import compression from "compression";
 
 const app = express();
+
+//Only allow CORS for our client
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+
+//Compresses text
+app.use(compression());
+
+//Rate limiter @250 api req/hour
+const limiter = rateLimit({
+	max: 250,
+	windowMs: 60 * 60 * 1000,
+	message: "Too many requests from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
+
+//Adds security headers
+app.use(helmet());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+//Adds helpful HTTP logging
 if (process.env.NODE_ENV === "development") {
 	app.use(morgan("dev"));
 }
 
-app.use(express.json());
+//Limit req.body to 100kb.
+app.use(
+	express.json({
+		limit: "100kb",
+	}),
+);
+
+//Prevents injection attacks and XSS attacks
+app.use(mongoSanitize());
+app.use(xss());
+
+//Parameter pollution
+app.use(hpp());
+
+//Parsing cookies and serving static images
 app.use(cookieParser());
 app.use(express.static(`${__dirname}/public`));
 
@@ -37,7 +76,7 @@ app.use("/api/v1/documents", documentRouter);
 app.use("/api/v1/stories", storyRouter);
 app.use("/api/v1/files", fileRouter);
 
-//if app reaches this route it must be a 404 error
+//If app reaches this route it must be a 404 error
 app.use((req, _, next) => {
 	next(new AppError(404, `Can't find ${req.originalUrl} on this server!`));
 });
