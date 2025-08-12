3c74f373f11a990edc3ff21552dd29b62ccbf5c4
Nicholas Bruno <165311112+Plasmatronics@users.noreply.github.com>
2025-08-12 17:20:18 -0400

    (improve) update gitignore

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
@@ -16,4 +19,4 @@ export default tseslint.config({
 		"@typescript-eslint/no-unused-vars": "warn",
 		"@typescript-eslint/no-explicit-any": "warn",
 	},
-});
+}, storybook.configs["flat/recommended"]);
