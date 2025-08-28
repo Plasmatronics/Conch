b509c1efdc8015b60d13f2f3ec848bfe8174bb72
Nicholas Bruno <165311112+Plasmatronics@users.noreply.github.com>
2025-08-28 19:43:01 -0400

    (feat) implement tree card

---
+++ client/src/components/Tree/TreeCard/TreeCard.types.ts
@@ -0,0 +1,18 @@
+import { ImageProps, CardRootProps, CardBodyProps } from "@chakra-ui/react";
+
+interface MemberData {
+	name: string;
+	numMemories: number;
+	birthYear: number;
+	deathYear?: number;
+	image: ImageProps["src"];
+}
+
+export interface TreeCardProps
+	extends Omit<CardRootProps, "size" | "width" | "height"> {
+	cardBodyProps?: CardBodyProps;
+	memberData: MemberData | MemberData[];
+	width: CardRootProps["width"];
+	height: CardRootProps["height"];
+	onMemoriesClick?: () => void;
+}
