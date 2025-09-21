5a18c5610c0176064814a12a5e4562bd1a039752
Nicholas Bruno <165311112+Plasmatronics@users.noreply.github.com>
2025-09-21 17:53:02 -0400

    memoization

--- client/src/components/Posts/DataPost/DataPost.tsx
+++ client/src/components/Posts/DataPost/DataPost.tsx
@@ -1,18 +1,17 @@
 import { useDataPost } from "./useDataPost";
 import { BasePost } from "../BasePost";
-import { useState } from "react";
-import { HydratedStoryDTO, HydratedFamilyTreeMemberDTO } from "@conch/shared";
+import React from "react";
+import { HydratedStoryDTO } from "@conch/shared";
 import { CommentSectionProps, IReply } from "../../Comments";
 
 export interface DataPostProps {
 	storyId: HydratedStoryDTO["id"];
-	personId: HydratedFamilyTreeMemberDTO["id"];
 }
 
 export const DataPost = ({ storyId }: DataPostProps) => {
-	const [isLiked, setIsLiked] = useState(false);
+	const [isLiked, setIsLiked] = React.useState(false);
 	const { storyQuery, avatarQuery } = useDataPost(storyId);
-	console.log(storyQuery.data);
+	const isLoading = storyQuery.isLoading || avatarQuery.isLoading;
 
 	const {
 		author = {
@@ -32,54 +31,68 @@ export const DataPost = ({ storyId }: DataPostProps) => {
 	} = storyQuery?.data ?? {};
 
 	const commentSectionData: CommentSectionProps["commentThreads"] =
-		comments?.map((commentThread) => {
-			const threadDTO = {
-				comment: {
-					comment: {
-						comment: commentThread.content,
-						user: commentThread.author.name,
-						avatar: avatarQuery.data?.get(commentThread.author.keyPhoto.fileKey)
-							?.downloadUrl,
-						datePosted: commentThread.createdAt,
-						relationship: commentThread.author.relationToRootMember,
-						loading: avatarQuery.isLoading || storyQuery.isLoading,
-						numLikes: commentThread.likes,
-					},
-
-					replies: commentThread?.replies.map((reply) => {
-						const replyDTO: IReply = {
+		React.useMemo(() => {
+			return (
+				comments?.map((commentThread) => {
+					const threadDTO = {
+						comment: {
 							comment: {
-								comment: reply.content,
-								user: reply.author.name,
-								avatar: avatarQuery.data?.get(reply.author.keyPhoto.fileKey)
-									?.downloadUrl,
-								datePosted: reply.createdAt,
-								relationship: reply.author.relationToRootMember,
+								comment: commentThread.content,
+								user: commentThread.author.name,
+								avatar: avatarQuery.data?.get(
+									commentThread.author.keyPhoto.fileKey,
+								)?.downloadUrl,
+								datePosted: commentThread.createdAt,
+								relationship: commentThread.author.relationToRootMember,
 								loading: avatarQuery.isLoading || storyQuery.isLoading,
-								numLikes: reply.likes,
+								numLikes: commentThread.likes,
 							},
-							replyingTo: commentThread.author.name,
-						};
-						return replyDTO;
-					}),
-				},
-			};
-			return threadDTO;
-		}) || [];
 
-	const typeSafeMedia = media.map((mediaItem) => {
-		return {
-			src: avatarQuery.data?.get(mediaItem.fileKey)?.downloadUrl || "default",
-			type: mediaItem.type || "image",
-		};
-	});
+							replies: commentThread?.replies.map((reply) => {
+								const replyDTO: IReply = {
+									comment: {
+										comment: reply.content,
+										user: reply.author.name,
+										avatar: avatarQuery.data?.get(reply.author.keyPhoto.fileKey)
+											?.downloadUrl,
+										datePosted: reply.createdAt,
+										relationship: reply.author.relationToRootMember,
+										loading: avatarQuery.isLoading || storyQuery.isLoading,
+										numLikes: reply.likes,
+									},
+									replyingTo: commentThread.author.name,
+								};
+								return replyDTO;
+							}),
+						},
+					};
+					return threadDTO;
+				}) || []
+			);
+		}, [avatarQuery.dataUpdatedAt, storyQuery.dataUpdatedAt, isLoading]);
+
+	const typeSafeMedia = React.useMemo(() => {
+		return media
+			.map((mediaItem) => {
+				const src = avatarQuery.data?.get(mediaItem.fileKey)?.downloadUrl;
+				const type = mediaItem.type;
+
+				if (src && type) {
+					return {
+						src,
+						type,
+					};
+				}
+			})
+			.filter(Boolean) as { src: string; type: "image" | "video" }[];
+	}, [avatarQuery.dataUpdatedAt, storyQuery.dataUpdatedAt, isLoading]);
 
 	return (
 		<BasePost
 			title={title}
 			text={content}
 			relationship={author.relationToRootMember}
-			loading={storyQuery.isLoading || avatarQuery.isLoading}
+			loading={isLoading}
 			user={author.name}
 			isLiked={isLiked}
 			media={typeSafeMedia}
